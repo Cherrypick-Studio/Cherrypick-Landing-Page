258f1683ebddf665fe8d6cc71361b30b8534a3d0
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2023-12-11 13:07:23 +0700

    feat: add robots.txt and sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://cherrypick.studio",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
