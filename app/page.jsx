4fb909a3b2a1fde708b2f0dd007d3a9505e0d6ab
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2023-12-18 10:03:58 +0700

    feat: add splash screen

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,35 @@
-import Clients from '@/components/organisms/clients'
-import Goals from '@/components/organisms/goals'
-import MainSection from '@/components/organisms/main-section'
-import Services from '@/components/organisms/services'
-import Works from '@/components/organisms/works'
+"use client";
+import Clients from "@/components/organisms/clients";
+import Goals from "@/components/organisms/goals";
+import MainSection from "@/components/organisms/main-section";
+import Services from "@/components/organisms/services";
+import Works from "@/components/organisms/works";
+import Lottie from "lottie-react";
+import splashLottie from "@/public/lotties/splash.json";
+import { m, LazyMotion, domAnimation } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className='flex flex-col md:gap-y-24 gap-y-8'>
-      <MainSection/>
-      <Goals/>
-      <Services/>
-      <Works/>
-      <Clients/>
-    </div>
-  )
+    <>
+      <LazyMotion features={domAnimation}>
+        <m.div
+          initial={{ opacity: 1, height: "100vh" }}
+          animate={{ opacity: 0, height: 0 }}
+          exit={{ y: -1000, opacity: 0, height: 0 }}
+          transition={{ ease: "easeOut", duration: 0.5, delay: 2 }}
+        >
+          <div className="flex items-center justify-center w-full h-screen bg-red-cherry-500 z-50">
+            <Lottie animationData={splashLottie} loop={false} />
+          </div>
+        </m.div>
+      </LazyMotion>
+      <div className="flex flex-col md:gap-y-24 gap-y-8">
+        <MainSection />
+        <Goals />
+        <Services />
+        <Works />
+        <Clients />
+      </div>
+    </>
+  );
 }
