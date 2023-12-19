46291052a586f497a748168bf2892b49eec674b5
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2023-12-19 17:43:31 +0700

    fix: change apostrophe

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -7,12 +7,12 @@ function Custom404() {
     <div className="flex items-center justify-center w-full h-screen bg-red-cherry-500 z-50">
       <LazyMotion features={domAnimation}>
         <m.div
-          // initial={{ opacity: 1, height: "100vh" }}
-          // animate={{ opacity: 0, height: 0 }}
-          // exit={{ y: -1000, opacity: 0, height: 0 }}
-          // transition={{ ease: "easeOut", duration: 0.5, delay: 2 }}
+        // initial={{ opacity: 1, height: "100vh" }}
+        // animate={{ opacity: 0, height: 0 }}
+        // exit={{ y: -1000, opacity: 0, height: 0 }}
+        // transition={{ ease: "easeOut", duration: 0.5, delay: 2 }}
         >
-          <h1 className="text-2xl text-white">Seems you're lost</h1>
+          <h1 className="text-2xl text-white">Seems you&apos;re lost</h1>
         </m.div>
       </LazyMotion>
     </div>
